415d5fe5eaf023e62fa611e619ade36ce73b0857
Alex Mellbye <82661985+Alex-Mellbye@users.noreply.github.com>
2022-04-21 10:56:39 +0200

    script.py

--- script.py
+++ script.py
@@ -9,12 +9,14 @@ from scipy.stats.mstats import winsorize
 import matplotlib.pyplot as plt
 import pyreadstat
 
-df = pd.read_spss(r'C:\Users\alex_\Documents\Anne Trana\Analysefil_BPD_Winsorizing.sav')
+# leser inn datafil
+df = pd.read_spss(r'C:\Users\alex_\Documents\Analysefil_BPD_Winsorizing.sav')
 
+# Sjekker kolonner og NaN
 print(df.columns)
-#print(df.isnull().sum())
+print(df.isnull().sum())
 
-# Winsorizing
+# Her gjør vi en 90% winsorzing (derav "0.05,0.05" - 5% av her ende)
 df["Win_NSSI_lifetime"] = winsorize(df["NSSI_lifetime"],(0.05,0.05))
 df["Win_SA_livstid"] = winsorize(df["SA_livstid"],(0.05,0.05))
 
@@ -23,41 +25,46 @@ print(df[["NSSI_lifetime", "Win_NSSI_lifetime"]].head(10))
 print(df[["SA_livstid", "Win_SA_livstid"]].head(10))
 
 # Sjekk for å se om gjennomsnittet forandrer seg veldig. Det skal det helst ikke gjøre for mye
+
 print("NSSI gjennomsnitt før Win")
 print(stat.mean(df["NSSI_lifetime"]))
 print("NSSI gjennomsnitt etter Win")
 print(stat.mean(df["Win_NSSI_lifetime"]))
+
 print("SA gjennomsnitt før Win")
 print(stat.mean(df["SA_livstid"]))
 print("SA gjennomsnitt etter Win")
 print(stat.mean(df["Win_SA_livstid"]))
 
 
+# Her visualiserer jeg dataen med boxplots
+# Dette viser hvordan rangen av verdier blir skrenket inn mens majoriteten av tilfeller holder seg på samme sted (mao at vi strammer inn de ytterste 5%'ene)
 
 plt.boxplot(df["NSSI_lifetime"])
-plt.title('NSSI_lifetime')
+plt.title('NSSI')
 plt.show()
 
 plt.boxplot(df["Win_NSSI_lifetime"])
-plt.title('Array with Outliers')
+plt.title('WIN_NSSI')
 plt.show()
 
 plt.boxplot(df["SA_livstid"])
-plt.title('Array with Outliers')
+plt.title('SA')
 plt.show()
 
 plt.boxplot(df["Win_SA_livstid"])
-plt.title('Array with Outliers')
+plt.title('WIN_SA')
 plt.show()
 
 
-Win = df[["Win_NSSI_lifetime", "Win_SA_livstid"]]
+# Linja under skriver ut det ferdige datasettet med de nye winsorizede variablene til den relevante mappa. Dette datasettet kan nå bli sendt til kunde
+pyreadstat.write_sav(df, r'C:\Users\alex_\Documents\BPD_Win.sav')
 
-pyreadstat.write_sav(df, r'C:\Users\alex_\Documents\Anne Trana\BPD_Win.sav')
-
-Win_check = pd.read_spss(r'C:\Users\alex_\Documents\Anne Trana\BPD_Win.sav')
+# Her laster jeg datasettet inn igjen bare for å være helt sikker på at det har alt det skal ha
+Win_check = pd.read_spss(r'C:\Users\alex_\Documents\BPD_Win.sav')
 print(Win_check.columns)
 
+# Her gjør jeg en kjapp øyesjekk
 print(Win_check[["NSSI_lifetime", "Win_NSSI_lifetime"]].head(10))
 print(Win_check[["SA_livstid", "Win_SA_livstid"]].head(10))
 
